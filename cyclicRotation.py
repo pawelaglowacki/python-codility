02a0cdd93a13c02f392731da2f93b068c5bd58f6
pawelaglowacki <pawelglowackicapgemini@gmail.com>
2017-04-22 09:48:44 +0200

    lesson2 cyclic rotation refactor + handling empty array

--- cyclicRotation.py
+++ cyclicRotation.py
@@ -3,8 +3,9 @@
 
 
 def solution(A,k):
-  left = A[0:-k]
-  right = A[-k:]
-  return right + left
+  try:
+    k = k%len(A)
+  except ZeroDivisionError: # to handle empty A array
+    k = 0
+  return A[-k:] + A[0:-k] #interchange left and right subarrays in A
 
-print solution([3, 8, 9, 7, 6], 2)
